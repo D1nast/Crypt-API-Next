85641cea116c4d66115a39a57c130c395afd72d6
Ishihara <almond1024r@gmail.com>
2025-02-18 00:28:48 +0900

    修正

--- src/models/index.d.ts
+++ src/models/index.d.ts
@@ -1,10 +1,5 @@
 import { ModelInit, MutableModel, __modelMeta__, ManagedIdentifier } from "@aws-amplify/datastore";
-// @ts-ignore
 import { LazyLoading, LazyLoadingDisabled } from "@aws-amplify/datastore";
-
-
-
-
 
 type EagerUser = {
   readonly [__modelMeta__]: {
